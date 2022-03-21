e9d84d575238b24537816a6f570af3afbb00b28c
EdvardsF <francs.edvards2002@gmail.com>
2022-03-21 14:33:27 +0200

    Add native functions

--- lox/interpreter/natives.py
+++ lox/interpreter/natives.py
@@ -26,4 +26,47 @@ class Input(Callable):
 
     def __repr__(self):
         return "<native function>"
+
+
+class String(Callable):
+    @property
+    def arity(self):
+        return 1
     
+    def call(self, interpreter, arguments: t.List[t.Any]):
+        return str(arguments[0])
+
+    def __repr__(self):
+        return "<native function>"
+    
+
+class Number(Callable):
+    @property
+    def arity(self):
+        return 1
+    
+    def call(self, interpreter, arguments: t.List[t.Any]):
+        return float(arguments[0])
+
+    def __repr__(self):
+        return "<native function>"
+
+
+class Type(Callable):
+    @property
+    def arity(self):
+        return 1
+    
+    def call(self, interpreter, arguments: t.List[t.Any]):
+        obj = arguments[0]
+        if isinstance(obj, float) or isinstance(obj, int):
+            return "number"
+        elif isinstance(obj, bool):
+            return "boolean"
+        elif isinstance(obj, str):
+            return "string"
+        else:
+            return "nil"
+
+    def __repr__(self):
+        return "<native function>" 
